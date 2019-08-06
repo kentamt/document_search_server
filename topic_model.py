d945101c39724d73ff78855b8b9fc0a92eee6357
kentam <kentabell0725@gmail.com>
2019-08-06 19:35:06 +0900

    update

--- topic_model.py
+++ topic_model.py
@@ -7,6 +7,8 @@
 # @date 3-Aug. 2019
 # 
 
+import pickle
+
 import time
 import logging
 from collections import Counter
@@ -26,22 +28,79 @@ from sklearn import datasets
 # Vizualization
 from wordcloud import WordCloud
 
+
+class Data:
+    def __init__(self, texts, docs, corpuses, flags):
+        self.texts = texts
+        self.docs = docs
+        self.corpuses = corpuses
+        self.is_trained = flags
+
+    def get(self, ind):
+        return self.texts[ind], self.docs[ind], self.corpuses[ind], self.is_trained[ind]
+
+    def get_doc(self, ind):
+        return self.docs[ind]
+
+    def get_text(self, ind):
+        return self.texts[ind]
+
+    def get_corpus(self, ind):
+        return self.corpuses[ind]
+
+    def get_is_trained(self, ind):
+        return self.is_trained[ind]
+
+    def add(self, text, doc, corpus, flag):
+        self.docs.append(doc)
+        self.texts.append(text)
+        self.corpuses.append(corpus)
+        self.is_trained.append(flag)
+
+    def set_trained(self, ind):
+        self.is_trained[ind] = True
+
+    def reset_trained(self, ind):
+        self.is_trained[ind] = False
+
 class TopicModel:
     def __init__(self):
         # member variables
         self.df = None  # DataFrame for original data ?
-        self.texts = None # 
+        
+        self.texts = None
         self.docs = None 
         self.corpus = None
+        self.trained_flags = None
+
         self.stopwords = None # TODO: install stopwords by nltk 
         self.lda = None
         self.num_topics = None
+        self.train_docs_ids = None
+        self.unknown_docs_ids = None
+        self.liked_doc_ids = None 
 
         # display training logs
         logging.basicConfig(format='%(message)s', level=logging.INFO)
 
-    def add_texts(self, texts):
+    def get_doc(self, ind):
+        return self.docs[ind]    
+
+    def add_doc(self, doc):
+        text = self.preprocess(doc)
+        curp = self.dictionary.doc2bow(text) # 辞書は学習につかう単語だけのものとする。レコメンドのときは既存の辞書をつかう
+        
+        self.docs.append(doc)
+        self.texts.append(text)
+        self.corpus.append(curp)
+        self.trained_flags.append(False)
+    
+    def add_docs(self, docs):
+        """
+        TODO: 複数の文献を一気に追加する
+        """
         pass
+
 
     def docs_to_texts(self, docs):
         """
@@ -51,15 +110,22 @@ class TopicModel:
         # return [[w for w in doc.lower().split() if w not in self.stopwords] for doc in docs]
         return [[w for w in doc.lower().split()] for doc in docs]
     
-    def create_corpus_from_doc(self, doc):
+    def corpus_from_doc(self, doc):
 
         text = self.preprocess(doc)
         corpus = self.dictionary.doc2bow(text)
         return corpus
 
+    def update_dictionary(self, texts):
+        """
+        trainingに使う単語の辞書
+        """
+        return gensim.corpora.Dictionary(texts)        
+
     def create_corpus_from_df(self, df):
         """
         function for test
+        TODO: use append instead of init list
         """
         num_docs = 100  # how many documents to use
 
@@ -67,9 +133,12 @@ class TopicModel:
 
         # self.texts = self.docs_to_texts(self.docs) # old ver
         self.texts = [self.preprocess(line) for line in self.docs] # remove stop words and lemmatization
+        
+        self.dictionary = self.update_dictionary(self.texts)
 
-        self.dictionary = gensim.corpora.Dictionary(self.texts)
         self.corpus = [self.dictionary.doc2bow(text) for text in self.texts]
+        self.trained_flags = [False] * num_docs
+        self.liked_doc_ids = [False] * num_docs
 
     def load_nltk_data(self):
         """
@@ -139,7 +208,16 @@ class TopicModel:
             eta = eta
         )
 
+        # update flags
+        self.trained_flags = [ e + True for e in self.trained_flags]
+
         return 1
+
+    def update_lda(self):
+        """
+        update LDA with new docs and trained 
+        """
+        pass
 
     def vizualize_result(self, figx=30, figy=30):
         plt.figure(figsize=(figx, figy))
@@ -157,7 +235,7 @@ class TopicModel:
             print(" - {}: {}".format(self.dictionary[t[0]], t[1]))
 
     def calc_topic_distribution_from_doc(self, doc):
-        test_corpus = self.create_corpus_from_doc(doc)
+        test_corpus = self.corpus_from_doc(doc)
         return sorted(self.lda.get_document_topics(test_corpus), key=lambda t:t[1], reverse=True)
 
     def disp_topic_distribution(self, doc):
@@ -178,7 +256,7 @@ class TopicModel:
         train_topic_distributions = self.lda.get_document_topics(self.corpus)
         self.doc_index = similarities.docsim.MatrixSimilarity(train_topic_distributions)
 
-    def recommend(self, doc):
+    def recommend(self, doc, num_recommended_docs = 3):
         """
         The class similarities.MatrixSimilarity is only appropriate when the whole set of vectors fits into memory. 
         For example, a corpus of one million documents would require 2GB of RAM in a 256-dimensional LSI space, when used with this class.
@@ -203,7 +281,10 @@ class TopicModel:
         # get similarity from all training corpus
         similar_corpus_id = self.doc_index.__getitem__(topic_dist)
         similar_corpus_id = sorted(enumerate(similar_corpus_id), key=lambda t: t[1], reverse=True) 
-        print(similar_corpus_id[:10])
+        recommended_docs_ids = [ e[0] for e in similar_corpus_id[:num_recommended_docs]]
+        print (recommended_docs_ids)
+        return recommended_docs_ids
+        
 
     def get_unused_texts(self):
         pass
@@ -212,17 +293,29 @@ class TopicModel:
         pass 
 
 if __name__ == "__main__":
+
+    use_pickle = True
+
     df = pd.read_csv("./arxivs_data.csv")
 
-    topic_model = TopicModel()
-    # topic_model.load_nltk_data()
-    topic_model.set_num_topics(5)
-    topic_model.create_corpus_from_df(df)
-    topic_model.train(num_pass=1)
-    topic_model.disp_topic_words(1)
-    
-    doc  = df.iloc[-1]["abstract"]
-    topic_model.disp_topic_distribution(doc)
-    topic_model.recommend(doc)
+    if not use_pickle:
+        topic_model = TopicModel()
+        topic_model.set_num_topics(5)
+        topic_model.create_corpus_from_df(df)
+        topic_model.train(num_pass=1)
+        with open("./topic_model.pickle", "wb") as f:
+            pickle.dump(topic_model, f)
 
-    print("OK")
+    with open("./topic_model.pickle", "rb") as f:
+        topic_model = pickle.load(f)
+        # topic_model.disp_topic_words(1)    
+        doc  = df.iloc[-1]["abstract"]
+        topic_model.disp_topic_distribution(doc)
+        for ind in topic_model.recommend(doc):
+            print(topic_model.get_doc(ind))
+            print("---")
+
+    
+    
+
+    print("Done")
